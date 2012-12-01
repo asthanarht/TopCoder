0fd4116c978b1b85c07d5004c6a7346bf2d1212d
safarisoul <shenlu.wang@gmail.com>
2012-12-01 16:48:41 +1100

    srm562 div1 250

---
+++ SRM 550-599/srm562/PastingPaintingDivOne.java
@@ -0,0 +1,23 @@
+package srm562;
+
+public class PastingPaintingDivOne {
+    public long[] countColors(String[] clipboard, int T) {
+        long[] count = new long[4];
+        int r = clipboard.length, c = clipboard[0].length();
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++) {
+                char ch = clipboard[i].charAt(j);
+                count["RGB.".indexOf(ch)] += ((i == 0 || j == 0) ? T : 1);
+                if (ch == '.')
+                    for (int move = 1; move < T && i + move < r && j + move < c; move++) {
+                        ch = clipboard[i + move].charAt(j + move);
+                        if (ch != '.') {
+                            count["RGB.".indexOf(ch)] += 
+                                    ((i == 0 || j == 0) ? T - move : 1);
+                            break;
+                        }
+                    }
+            }
+        return new long[] { count[0], count[1], count[2] };
+    }
+}
