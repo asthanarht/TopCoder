6233656060abdb999ddbfa6d2bfd1e7891930e86
safarisoul <shenlu.wang@gmail.com>
2013-02-07 16:57:45 +1100

    srm569 div1 250 bitwise 500 greedy || mask

---
+++ SRM 550-599/srm569/TheDevice.java
@@ -0,0 +1,22 @@
+package srm569;
+
+public class TheDevice {
+    public int minimumAdditional(String[] plates) {
+        int result = 0;
+        for (int i = 0; i < plates[0].length(); i++) {
+            int count0 = 0, count1 = 0;
+            for (int j = 0; j < plates.length; j++)
+                if (plates[j].charAt(i) == '0')
+                    count0++;
+                else
+                    count1++;
+            int need = 0;
+            if (count0 < 1)
+                need += 1;
+            if (count1 < 2)
+                need += 2 - count1;
+            result = Math.max(result, need);
+        }
+        return result;
+    }
+}
---
+++ SRM 550-599/srm569/TheJediTest.java
@@ -0,0 +1,55 @@
+package srm569;
+
+public class TheJediTest {
+    public int minimumSupervisors(int[] students, int K) {
+        if (System.currentTimeMillis() % 2 == 0)
+            return minimumSupervisors_mask(students, K);
+        else
+            return minimumSupervisors_greedy(students, K);
+    }
+
+    public int minimumSupervisors_mask(int[] students, int K) {
+        int n = students.length;
+        int result = Integer.MAX_VALUE;
+        for (int mask = 0; mask < (1 << n - 1); mask++) {
+            int[] student = students.clone();
+            for (int i = 0; i < n - 1; i++)
+                if (((1 << i) & mask) == 0) {
+                    int extra = Math.min(students[i], student[i] % K);
+                    student[i] -= extra;
+                    student[i + 1] += extra;
+                }
+                else {
+                    int move = Math.min(students[i + 1], K - student[i] % K);
+                    student[i] += move;
+                    student[i + 1] -= move;
+                }
+            int jedi = 0;
+            for (int i = 0; i < n; i++)
+                jedi += (student[i] + K - 1) / K;
+            result = Math.min(result, jedi);
+        }
+        return result;
+    }
+
+    public static int minimumSupervisors_greedy(int[] students, int K) {
+        int n = students.length;
+        int[] fromDown = new int[n];
+        for (int i = 0; i < n - 1; i++) {
+            int extra = (students[i] + fromDown[i]) % K;
+            if (students[i] >= extra) {
+                fromDown[i + 1] += extra;
+                students[i] -= extra;
+                continue;
+            }
+            int need = K - (students[i] + fromDown[i]) % K;
+            int move = Math.min(need, students[i + 1]);
+            students[i + 1] -= move;
+            students[i] += move;
+        }
+        int result = 0;
+        for (int i = 0; i < n; i++)
+            result += (students[i] + fromDown[i] + K - 1) / K;
+        return result;
+    }
+}
