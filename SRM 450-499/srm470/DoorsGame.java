ccb3e033977ea7d0702c421b641016ff4cb2fb93
safarisoul <shenlu.wang@gmail.com>
2013-02-20 02:32:12 +1100

    srm470 div1 250 greedy simulation 500 combination

---
+++ SRM 450-499/srm470/DoorsGame.java
@@ -0,0 +1,43 @@
+package srm470;
+
+public class DoorsGame {
+    public int determineOutcome(String doors, int trophy) {
+        boolean[] doorJohn = new boolean[26];
+        boolean[] doorGogo = new boolean[26];
+        for (int i = 0; i < doors.length(); i++)
+            if (i < trophy)
+                doorJohn[doors.charAt(i) - 'A'] = true;
+            else
+                doorGogo[doors.charAt(i) - 'A'] = true;
+        int countJohn = 0, countGogo = 0, common = 0;
+        for (int i = 0; i < 26; i++)
+            if (doorJohn[i] && doorGogo[i])
+                common++;
+            else if (doorJohn[i] && !doorGogo[i])
+                countJohn++;
+            else if (!doorJohn[i] && doorGogo[i])
+                countGogo++;
+        int move = 0;
+        while (countJohn + common > 0 && countGogo + common > 0) {
+            if (move % 2 == 0) {
+                if (countJohn > 0)
+                    countJohn--;
+                else
+                    common--;
+            }
+            else {
+                if (countGogo > 0)
+                    countGogo--;
+                else
+                    common--;
+            }
+            move++;
+        }
+        if (countJohn + common == 0) {
+            if (countGogo > 0)
+                return move;
+            return 0;
+        }
+        return move * -1;
+    }
+}
---
+++ SRM 450-499/srm470/DrawingLines.java
@@ -0,0 +1,38 @@
+package srm470;
+
+public class DrawingLines {
+    public double countLineCrossings(int n, int[] startDot, int[] endDot) {
+        double result = 0;
+        int m = startDot.length;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < m; j++)
+                if (startDot[i] < startDot[j] && endDot[i] > endDot[j])
+                    result++;
+        boolean[] start = new boolean[n + 1];
+        boolean[] end = new boolean[n + 1];
+        for (int i = 0; i < m; i++) {
+            start[startDot[i]] = true;
+            end[endDot[i]] = true;
+        }
+        for (int i = 0; i < m; i++) {
+            long startLeft = startDot[i] - 1;
+            long startRight = n - startDot[i];
+            long endLeft = endDot[i] - 1;
+            long endRight = n - endDot[i];
+            for (int j = 0; j < m; j++) {
+                if (startDot[j] < startDot[i])
+                    startLeft--;
+                else if (startDot[j] > startDot[i])
+                    startRight--;
+                if (endDot[j] < endDot[i])
+                    endLeft--;
+                else if (endDot[j] > endDot[i])
+                    endRight--;
+            }
+            result += (startLeft * endRight + startRight * endLeft)
+                    / (double) (n - m);
+        }
+        result += (n - m) * (n - m - 1) / 4.0;
+        return result;
+    }
+}
