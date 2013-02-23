1f73bc08f74ab2c8093fdb45b007eaa33f18ff76
safarisoul <shenlu.wang@gmail.com>
2013-02-24 06:57:48 +1100

    TCO2013 r1A div1 250

---
+++ TCO/TCO 2013/r1A/HouseBuilding.java
@@ -0,0 +1,21 @@
+package r1A;
+
+public class HouseBuilding {
+    public int getMinimum(String[] area) {
+        int result = Integer.MAX_VALUE;
+        int n = area.length, m = area[0].length();
+        for (int level = 0; level < 9; level++) {
+            int total = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++) {
+                    int num = area[i].charAt(j) - '0';
+                    if (num > level + 1)
+                        total += num - level - 1;
+                    else if (num < level)
+                        total += level - num;
+                }
+            result = Math.min(result, total);
+        }
+        return result;
+    }
+}
