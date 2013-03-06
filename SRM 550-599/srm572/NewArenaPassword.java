4f74b8999d2fbb05cfab5f3cd4fbf3ba64ce11df
safarisoul <shenlu.wang@gmail.com>
2013-03-07 02:14:34 +1100

    srm572 div1 250

---
+++ SRM 550-599/srm572/NewArenaPassword.java
@@ -0,0 +1,33 @@
+package srm572;
+
+public class NewArenaPassword {
+    public int minChange(String oldPassword, int K) {
+        int n = oldPassword.length();
+        if (K == n)
+            return 0;
+        char[] string = oldPassword.toCharArray();
+        int result = 0;
+        boolean[] check = new boolean[n];
+        for (int i = 0; i < K; i++)
+            if (!check[i]) {
+                int num = 0;
+                int[] count = new int[26];
+                int index = i, match = index + n - K;
+                while (index < K) {
+                    count[string[index] - 'a']++;
+                    check[index] = true;
+                    num++;
+                    index = match;
+                    match = index + n - K;
+                }
+                count[string[index] - 'a']++;
+                check[index] = true;
+                num++;
+                int max = 0;
+                for (int j = 0; j < 26; j++)
+                    max = Math.max(max, count[j]);
+                result += num - max;
+            }
+        return result;
+    }
+}
