2ca931dffab42a42238ad02f6ee8144e27efe487
safarisoul <shenlu.wang@gmail.com>
2013-03-23 15:47:16 +1100

    TCO2012 r1A div1 500 math

---
+++ TCO/TCO 2012/r1A/EllysFractions.java
@@ -0,0 +1,22 @@
+package r1A;
+
+import java.util.Arrays;
+
+public class EllysFractions {
+    public long getCount(int N) {
+        boolean[] prime = new boolean[N + 1];
+        Arrays.fill(prime, true);
+        for (int i = 2; i <= N; i++)
+            if (prime[i])
+                for (int j = i + i; j <= N; j += i)
+                    prime[j] = false;
+        long result = 0;
+        for (int i = 2; i <= N; i++) {
+            int count = 0;
+            for (int j = 3; j <= i; j++)
+                count += prime[j] ? 1 : 0;
+            result += 1L << count;
+        }
+        return result;
+    }
+}
