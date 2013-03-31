d94ca9c5339debcce6475849d3435549f4736cf6
safarisoul <shenlu.wang@gmail.com>
2013-04-01 02:30:01 +1100

    TCO2012 r2B div1 300

---
+++ TCO/TCO 2012/r2B/BlurredDartboard.java
@@ -0,0 +1,36 @@
+package r2B;
+
+public class BlurredDartboard {
+    public int minThrows(int[] points, int P) {
+        int sum = points.length, max = 0, count = 0;
+        boolean[] num = new boolean[points.length + 1];
+        for (int i = 0; i < points.length; i++) {
+            max = Math.max(points[i], max);
+            sum += i;
+            if (points[i] == 0)
+                count++;
+            else {
+                sum -= points[i];
+                num[points[i]] = true;
+            }
+        }
+        if (count * max >= sum)
+            return (P + max - 1) / max;
+        else {
+            int res = count * (P / sum);
+            P = P % sum;
+            int n = Integer.MAX_VALUE;
+            if (max != 0)
+                n = (P + max - 1) / max;
+            int plus = 0, need = 0;
+            for (int i = 1; i <= points.length; i++)
+                if (!num[i]) {
+                    if (plus >= P)
+                        break;
+                    plus += i;
+                    need++;
+                }
+            return Math.min(need, n) + res;
+        }
+    }
+}
