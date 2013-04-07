eb7983643e1bd855f5af431042f5577e140a28f7
safarisoul <shenlu.wang@gmail.com>
2013-04-07 15:18:27 +1000

    srm575 div1 500 probability

---
+++ SRM 550-599/srm575/TheSwapsDivOne.java
@@ -0,0 +1,49 @@
+package srm575;
+
+public class TheSwapsDivOne {
+    public double find(String[] sequence, int k) {
+        String seq = "";
+        for (int i = 0; i < sequence.length; i++)
+            seq += sequence[i];
+        int n = seq.length();
+
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++)
+            num[i] = seq.charAt(i) - '0';
+
+        double caseNum = c2(n);
+        double stayCase = c2(n - 1);
+
+        double pSame = 1;
+        double pOther = 0;
+        for (int turn = 0; turn < k; turn++) {
+            pSame = (pSame * stayCase + pOther) / caseNum;
+            pOther = 1 - pSame;
+        }
+        double pAnother = pOther / (n - 1);
+
+        double[] numExpect = new double[n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (i == j)
+                    numExpect[j] += num[i] * pSame;
+                else
+                    numExpect[j] += num[i] * pAnother;
+
+        int[] outCase = new int[n];
+        for (int i = 0; i < n; i++)
+            outCase[i] = (i + 1) * (n - i);
+        int totalCase = n * (n + 1) / 2;
+
+        double expect = 0;
+        for (int i = 0; i < n; i++)
+            expect += numExpect[i] * outCase[i];
+        expect /= totalCase;
+
+        return expect;
+    }
+
+    private double c2(int n) {
+        return n * (n - 1) / 2;
+    }
+}
