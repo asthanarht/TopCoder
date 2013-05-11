2e265bd6e6c77da1d90e6041b50c03044f9dcedb
safarisoul <shenlu.wang@gmail.com>
2013-05-11 21:04:19 +1000

    srm483 div1 250

---
+++ SRM 450-499/srm483/BestApproximationDiv1.java
@@ -0,0 +1,34 @@
+package srm483;
+
+public class BestApproximationDiv1 {
+    private static char[] num;
+
+    public static String findFraction(int maxDen, String number) {
+        num = number.toCharArray();
+        int quality = 0;
+        int bestD = 1, bestN = 0;
+        for (int denominator = 1; denominator <= maxDen; denominator++)
+            for (int numerator = 0; numerator < denominator; numerator++) {
+                int q = quality(numerator, denominator);
+                if (q > quality) {
+                    quality = q;
+                    bestD = denominator;
+                    bestN = numerator;
+                }
+            }
+        return bestN + "/" + bestD + " has " + quality + " exact digits";
+    }
+
+    private static int quality(double numerator, double denominator) {
+        int quality = 1;
+        String fraction = Double.toString(numerator / denominator);
+        for (int i = fraction.length(); i < 8; i++)
+            fraction += '0';
+        for (int i = 2; i < 8; i++)
+            if (fraction.charAt(i) == num[i])
+                quality++;
+            else
+                break;
+        return quality;
+    }
+}
