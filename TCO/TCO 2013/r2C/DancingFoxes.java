f0255e9d62b331869b4007af734b936df1b2585d
safarisoul <shenlu.wang@gmail.com>
2013-05-12 04:08:21 +1000

    TCO2013 r2C div1 250 floyd

---
+++ TCO/TCO 2013/r2C/DancingFoxes.java
@@ -0,0 +1,28 @@
+package r2C;
+
+public class DancingFoxes {
+    public int minimalDays(String[] friendship) {
+        int n = friendship.length;
+        int[][] relation = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (friendship[i].charAt(j) == 'Y')
+                    relation[i][j] = 1;
+                else
+                    relation[i][j] = 100000;
+        for (int k = 0; k < n; k++)
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (relation[i][k] >= 0 && relation[k][j] >= 0)
+                        relation[i][j] = Math.min(relation[i][j],
+                                relation[i][k] + relation[k][j]);
+        if (relation[0][1] == 100000)
+            return -1;
+        int result = 0, nodes = relation[0][1] + 1;
+        while (nodes > 3) {
+            result++;
+            nodes -= nodes / 3;
+        }
+        return result + (nodes == 3 ? 1 : 0);
+    }
+}
