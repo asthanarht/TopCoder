32beac99b18951d1e7c54322f19a66dec62158c6
safarisoul <shenlu.wang@gmail.com>
2013-06-04 10:09:53 +1000

    srm480 div2 900 greedy

---
+++ SRM 450-499/srm480/SignalIntelligence.java
@@ -0,0 +1,22 @@
+package srm480;
+
+import java.util.Arrays;
+
+public class SignalIntelligence {
+    public long encrypt(int[] numbers) {
+        Arrays.sort(numbers);
+        int n = numbers.length;
+        long min = Long.MAX_VALUE;
+        for (int last = 0; last < n; last++) {
+            long p = 1;
+            for (int i = 0; i < n; i++)
+                if (i != last) {
+                    long end = p + numbers[i];
+                    while (p <= end)
+                        p *= 2;
+                }
+            min = Math.min(min, p + numbers[last] - 1);
+        }
+        return min;
+    }
+}
