48b5163f18f13a2b1e0ef7ee9792a940e865b02d
safarisoul <shenlu.wang@gmail.com>
2013-06-13 22:49:17 +1000

    srm481 div2 500

---
+++ SRM 450-499/srm481/ChickenOracle.java
@@ -0,0 +1,46 @@
+package srm481;
+
+public class ChickenOracle {
+    public String theTruth(int n, int eggCount, int lieCount, int liarCount) {
+        int chickCount = n - eggCount;
+        int countTrue = n - lieCount, countFalse = lieCount;
+        int trueToFalse, falseToTrue;
+        // if "The chicken"
+        boolean theChicken = false;
+        {
+            trueToFalse = Math.max(0, eggCount - countFalse);
+            falseToTrue = Math.max(0, chickCount - countTrue);
+            int change = trueToFalse + falseToTrue;
+            if (change == liarCount)
+                theChicken = true;
+            if (change < liarCount
+                    && (liarCount - change) % 2 == 0
+                    && Math.min(countTrue - trueToFalse, countFalse
+                            - falseToTrue) * 2 >= liarCount - change)
+                theChicken = true;
+        }
+        // if "The egg"
+        boolean theEgg = false;
+        {
+            trueToFalse = Math.max(0, chickCount - countFalse);
+            falseToTrue = Math.max(0, eggCount - countTrue);
+            int change = trueToFalse + falseToTrue;
+            if (change == liarCount)
+                theEgg = true;
+            if (change < liarCount
+                    && (liarCount - change) % 2 == 0
+                    && Math.min(countTrue - trueToFalse, countFalse
+                            - falseToTrue) * 2 >= liarCount - change)
+                theEgg = true;
+        }
+        // result
+        if (theEgg && !theChicken)
+            return "The egg";
+        else if (!theEgg && theChicken)
+            return "The chicken";
+        else if (theEgg && theChicken)
+            return "Ambiguous";
+        else
+            return "The oracle is a lie";
+    }
+}
---
+++ SRM 450-499/srm481/CircleMarket.java
@@ -0,0 +1,25 @@
+package srm481;
+
+public class CircleMarket {
+    public int makePurchases(int[] openTime, int[] closeTime, int travelTime) {
+        int n = openTime.length;
+        boolean[] visited = new boolean[n];
+        int result = 0;
+        int position = 0;
+        int time = 0;
+        do {
+            if (!visited[position] && time >= openTime[position]
+                    && time <= closeTime[position]) {
+                result++;
+                visited[position] = true;
+            }
+            else {
+                position++;
+                if (position == n)
+                    position = 0;
+                time += travelTime;
+            }
+        } while (result < n && time <= 1000000);
+        return result;
+    }
+}
