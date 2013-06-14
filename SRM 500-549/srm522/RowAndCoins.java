4303102ecf8473df22e66b739fd574b22248905d
safarisoul <shenlu.wang@gmail.com>
2013-06-14 15:53:00 +1000

    srm522 div1 250

---
+++ SRM 500-549/srm522/RowAndCoins.java
@@ -0,0 +1,10 @@
+package srm522;
+
+public class RowAndCoins {
+    public String getWinner(String cells) {
+        char first = cells.charAt(0), last = cells.charAt(cells.length() - 1);
+        if (first == 'B' && last == 'B')
+            return "Bob";
+        return "Alice";
+    }
+}
