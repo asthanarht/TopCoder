2632b9fa2dc7af97a304ae7d8e7226be45016d1e
safarisoul <shenlu.wang@gmail.com>
2013-06-14 15:25:51 +1000

    srm547 div2 500 dp

---
+++ SRM 500-549/srm547/PillarsDivTwo.java
@@ -0,0 +1,23 @@
+package srm547;
+
+public class PillarsDivTwo {
+    public double maximalLength(int[] height, int w) {
+        int n = height.length;
+        int maxH = 100;
+        double[][] dp = new double[n][maxH + 1];
+        for (int i = 0; i < n - 1; i++) {
+            for (int h1 = 1; h1 <= height[i]; h1++)
+                for (int h2 = 1; h2 <= height[i + 1]; h2++)
+                    dp[i + 1][h2] = Math.max(dp[i + 1][h2],
+                            dp[i][h1] + length(Math.abs(h2 - h1), w));
+        }
+        double max = dp[n - 1][1];
+        for (int h = 2; h <= height[n - 1]; h++)
+            max = Math.max(max, dp[n - 1][h]);
+        return max;
+    }
+
+    private double length(int h, int w) {
+        return Math.sqrt(h * h + w * w);
+    }
+}
