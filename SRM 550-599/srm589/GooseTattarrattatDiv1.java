7f112e50bf2047a992fcd6decff9aa9c994d5e2f
safarisoul <shenlu.wang@gmail.com>
2013-08-27 23:54:52 +1000

    srm589 div1 250 floyd

---
+++ SRM 550-599/srm589/GooseTattarrattatDiv1.java
@@ -0,0 +1,37 @@
+package srm589;
+
+public class GooseTattarrattatDiv1 {
+    public int getmin(String S) {
+        int[] cnt = new int[26];
+        for (int i = 0; i < S.length(); i++)
+            cnt[S.charAt(i) - 'a']++;
+
+        boolean[][] same = new boolean[26][26];
+        for (int left = 0, right = S.length() - 1; left < right; left++, right--) {
+            int cleft = S.charAt(left) - 'a';
+            int cright = S.charAt(right) - 'a';
+            same[cleft][cright] = same[cright][cleft] = true;
+        }
+        for (int c = 0; c < 26; c++)
+            same[c][c] = true;
+
+        for (int k = 0; k < 26; k++)
+            for (int i = 0; i < 26; i++)
+                for (int j = 0; j < 26; j++)
+                    same[i][j] |= same[i][k] && same[k][j];
+
+        int res = 0;
+        for (int c1 = 0; c1 < 26; c1++)
+            if (cnt[c1] > 0) {
+                int max = 0;
+                for (int c2 = 0; c2 < 26; c2++)
+                    if (same[c1][c2]) {
+                        res += cnt[c2];
+                        max = Math.max(max, cnt[c2]);
+                        cnt[c2] = 0;
+                    }
+                res -= max;
+            }
+        return res;
+    }
+}
