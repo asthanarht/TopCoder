e90a0beb00ee9d8bb7df752dacf75d930ce0cc98
safarisoul <shenlu.wang@gmail.com>
2013-09-08 09:58:23 +1000

    srm590 div1 250

---
+++ SRM 550-599/srm590/FoxAndChess.java
@@ -0,0 +1,38 @@
+package srm590;
+
+public class FoxAndChess {
+    public String ableToMove(String begin, String target) {
+        boolean ok = true;
+        int step = 1;
+        do {
+            int from = find(step, begin);
+            if (from == -1)
+                break;
+            int to = find(step, target);
+            if (to == -1) {
+                ok = false;
+                break;
+            }
+            if (begin.charAt(from) != target.charAt(to))
+                ok = false;
+            if (from < to && begin.charAt(from) == 'L')
+                ok = false;
+            if (from > to && begin.charAt(from) == 'R')
+                ok = false;
+            step++;
+        } while (ok);
+        if (find(step, target) != -1)
+            ok = false;
+        return ok ? "Possible" : "Impossible";
+    }
+
+    private int find(int n, String string) {
+        for (int count = 0, i = 0; i < string.length(); i++)
+            if (string.charAt(i) != '.') {
+                count++;
+                if (count == n)
+                    return i;
+            }
+        return -1;
+    }
+}
