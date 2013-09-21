643bdcd00baf14fa1dbb6a8067a93fed66d126b1
safarisoul <shenlu.wang@gmail.com>
2013-09-22 09:20:01 +1000

    srm587 div1 250

---
+++ SRM 550-599/srm587/JumpFurther.java
@@ -0,0 +1,13 @@
+package srm587;
+
+public class JumpFurther {
+    public int furthest(int N, int badStep) {
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+            if (sum == badStep)
+                sum -= 1;
+        }
+        return sum;
+    }
+}
