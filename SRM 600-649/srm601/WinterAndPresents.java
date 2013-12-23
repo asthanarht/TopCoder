1e6a6d0cc664d2c5c0bd92246b59c87e940e8a65
safarisoul <shenlu.wang@gmail.com>
2013-12-23 11:29:19 +1100

    srm601 div1 250

---
+++ SRM 600-649/srm601/WinterAndPresents.java
@@ -0,0 +1,21 @@
+package srm601;
+
+public class WinterAndPresents {
+	public long getNumber(int[] apple, int[] orange) {
+		int n = apple.length;
+		long res = 0;
+		loop: for (int x = 1; x <= 2000000; x++) {
+			int min = 0, max = 0;
+			for (int i = 0; i < n; i++)
+				if (apple[i] + orange[i] < x)
+					break loop;
+				else {
+					if (x > orange[i])
+						min += x - orange[i];
+					max += Math.min(apple[i], x);
+				}
+			res += max - min + 1;
+		}
+		return res;
+	}
+}
