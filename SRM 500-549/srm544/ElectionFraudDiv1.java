67776d0d5791405183bfd92febf10c6ba54f2846
safarisoul <shenlu.wang@gmail.com>
2014-01-01 00:52:23 +1100

    srm544 div1 275

---
+++ SRM 500-549/srm544/ElectionFraudDiv1.java
@@ -0,0 +1,37 @@
+package srm544;
+
+public class ElectionFraudDiv1 {
+    public int MinimumVoters(int[] percentages) {
+        long start = System.currentTimeMillis();
+        for (int total = 1; total < Integer.MAX_VALUE; total++) {
+            long end = System.currentTimeMillis();
+            if (end - start >= 1800)
+                break;
+            if (check(percentages, total))
+                return total;
+        }
+        return -1;
+    }
+
+    public boolean check(int[] percentages, int total) {
+        int n = percentages.length;
+        int[] min = new int[n], max = new int[n];
+        for (int i = 0; i < n; i++) {
+            min[i] = Math.max(0,
+                    (int) (total * (percentages[i] - 0.5) * 0.01) - 1);
+            while (Math.round(100.0 * min[i] / total) < percentages[i])
+                min[i]++;
+            max[i] = (int) (total * (percentages[i] + 0.5) * 0.01) + 1;
+            while (Math.round(100.0 * max[i] / total) > percentages[i])
+                max[i]--;
+        }
+        int summin = 0, summax = 0;
+        for (int i = 0; i < n; i++) {
+            summin += min[i];
+            summax += max[i];
+        }
+        if (summin <= total && summax >= total)
+            return true;
+        return false;
+    }
+}
---
+++ SRM 500-549/srm544/ElectionFraudDiv2.java
@@ -0,0 +1,20 @@
+package srm544;
+
+public class ElectionFraudDiv2 {
+    public String IsFraudulent(int[] percentages) {
+        int n = percentages.length;
+        int[] min = new int[n], max = new int[n];
+        for (int i = 0; i < n; i++) {
+            min[i] = Math.max(0, 100 * percentages[i] - 50);
+            max[i] = 100 * percentages[i] + 49;
+        }
+        int summin = 0, summax = 0;
+        for (int i = 0; i < n; i++) {
+            summin += min[i];
+            summax += max[i];
+        }
+        if (summin <= 10000 && summax >= 10000)
+            return "NO";
+        return "YES";
+    }
+}
