ded66d389dc44fde1189cc47d1309220bfeaae2f
safarisoul <shenlu.wang@gmail.com>
2014-01-22 00:57:06 +1100

    srm605 div1 250

---
+++ SRM 600-649/srm605/AlienAndHamburgers.java
@@ -0,0 +1,48 @@
+package srm605;
+
+public class AlienAndHamburgers {
+	public int getNumber(int[] type, int[] taste) {
+		int n = type.length;
+		int Y = 0, A = 0;
+		boolean[] select = new boolean[n];
+		int[] tastes = new int[101];
+		for (int i = 0; i < n; i++)
+			if (taste[i] > 0) {
+				tastes[type[i]] += taste[i];
+				select[i] = true;
+			}
+		for (int i = 0; i < n; i++)
+			if (tastes[type[i]] > 0)
+				select[i] = true;
+		for (int i = 1; i <= 100; i++)
+			if (tastes[i] > 0) {
+				Y++;
+				A += tastes[i];
+			}
+		while (true) {
+			int best = -1000000;
+			int besti = -1;
+			boolean found = false;
+			for (int i = 0; i < n; i++)
+				if (!select[i]) {
+					if (taste[i] > best) {
+						best = taste[i];
+						besti = i;
+						found = true;
+					}
+				}
+			if (found) {
+				if ((Y + 1) * (A + best) >= Y * A) {
+					Y++;
+					A += best;
+					for (int i = 0; i < n; i++)
+						if (type[i] == type[besti])
+							select[i] = true;
+					continue;
+				}
+			}
+			break;
+		}
+		return Y * A;
+	}
+}
