3c235735fe39eafd722f1bf7f22f90be14b44636
safarisoul <shenlu.wang@gmail.com>
2014-02-23 12:56:04 +1100

    srm608 div1 300

---
+++ SRM 600-649/srm608/MysticAndCandies.java
@@ -0,0 +1,26 @@
+package srm608;
+
+import java.util.Arrays;
+
+public class MysticAndCandies {
+
+	public int minBoxes(int C, int X, int[] low, int[] high) {
+		int n = low.length;
+		Arrays.sort(low);
+		int sum = 0, res = n;
+		for (int i = n - 1; i >= 0; i--) {
+			sum += low[i];
+			if (sum >= X)
+				res = Math.min(res, n - i);
+		}
+		Arrays.sort(high);
+		sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += high[i];
+			if (C - sum >= X)
+				res = Math.min(res, n - 1 - i);
+		}
+		return res;
+	}
+
+}
