85ba2c809c30d38c377f2e3d377eb81fae6781f4
safarisoul <shenlu.wang@gmail.com>
2014-02-26 15:29:03 +1100

    srm610 div1 250 550 dp

---
+++ SRM 600-649/srm610/AlbertoTheAviator.java
@@ -0,0 +1,53 @@
+package srm610;
+
+import java.util.Arrays;
+
+public class AlbertoTheAviator {
+	public int MaximumFlights(int F, int[] duration, int[] refuel) {
+		int n = duration.length;
+		Mission[] mission = new Mission[n];
+		for (int i = 0; i < n; i++)
+			mission[i] = new Mission(duration[i], refuel[i]);
+		Arrays.sort(mission);
+
+		int[][] dp = new int[n][n + 1];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j <= n; j++)
+				dp[i][j] = -1;
+		dp[0][0] = F;
+		if (F >= mission[0].duration)
+			dp[0][1] = F - mission[0].lose;
+
+		for (int i = 0; i < n - 1; i++)
+			for (int done = 0; done < n; done++) {
+				dp[i + 1][done] = Math.max(dp[i + 1][done], dp[i][done]);
+				if (dp[i][done] >= mission[i + 1].duration) {
+					dp[i + 1][done + 1] = Math.max(dp[i + 1][done + 1],
+							dp[i][done] - mission[i + 1].lose);
+				}
+			}
+
+		int res = 0;
+		for (int done = 0; done <= n; done++)
+			if (dp[n - 1][done] >= 0)
+				res = done;
+		return res;
+	}
+}
+
+class Mission implements Comparable<Mission> {
+	int duration;
+	int refuel;
+	int lose;
+
+	public Mission(int duration, int refuel) {
+		super();
+		this.duration = duration;
+		this.refuel = refuel;
+		lose = duration - refuel;
+	}
+
+	public int compareTo(Mission m) {
+		return m.refuel - this.refuel;
+	}
+}
---
+++ SRM 600-649/srm610/TheMatrix.java
@@ -0,0 +1,36 @@
+package srm610;
+
+public class TheMatrix {
+	public int MaxArea(String[] board) {
+		int m = board.length, n = board[0].length();
+		int max = 1;
+		int[][][] dp = new int[m][n][n];
+		int[][] cntc = new int[m][n];
+		int[][] cntr = new int[m][n];
+		for (int row = 0; row < m; row++) {
+			for (int col = 0; col < n; col++) {
+				if (row > 0
+						&& board[row].charAt(col) != board[row - 1].charAt(col))
+					cntc[row][col] = cntc[row - 1][col] + 1;
+				else
+					cntc[row][col] = 1;
+				if (col > 0
+						&& board[row].charAt(col) != board[row].charAt(col - 1))
+					cntr[row][col] = cntr[row][col - 1] + 1;
+				else
+					cntr[row][col] = 1;
+				for (int from = 0; from <= col; from++) {
+					int width = col - from + 1;
+					if (width > cntr[row][col])
+						continue;
+					dp[row][from][col] = 1;
+					max = Math.max(max, width);
+					if (cntc[row][col] > 1)
+						dp[row][from][col] = dp[row - 1][from][col] + 1;
+					max = Math.max(max, width * dp[row][from][col]);
+				}
+			}
+		}
+		return max;
+	}
+}
