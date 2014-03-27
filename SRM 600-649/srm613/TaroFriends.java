8e94d4a55473916a35393e1c196f73cb4ef9d961
safarisoul <shenlu.wang@gmail.com>
2014-03-27 22:10:05 +1100

    srm613 div1 250

---
+++ SRM 600-649/srm613/TaroFriends.java
@@ -0,0 +1,21 @@
+package srm613;
+
+import java.util.Arrays;
+
+public class TaroFriends {
+
+	public int getNumber(int[] coordinates, int X) {
+		Arrays.sort(coordinates);
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < coordinates.length; i++) {
+			int left = coordinates[0] + X, right = coordinates[i] + X;
+			for (int j = i + 1; j < coordinates.length; j++) {
+				left = Math.min(left, coordinates[j] - X);
+				right = Math.max(right, coordinates[j] - X);
+			}
+			min = Math.min(min, right - left);
+		}
+		return min;
+	}
+
+}
