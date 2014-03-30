b61b5b98d61a7128747740311b7a8ecb68dfedba
safarisoul <shenlu.wang@gmail.com>
2014-03-30 11:53:18 +1100

    srm614 div1 250 range sum

---
+++ SRM 600-649/srm614/MinimumSquare.java
@@ -0,0 +1,29 @@
+package srm614;
+
+import java.util.Arrays;
+
+public class MinimumSquare {
+
+	public long minArea(int[] x, int[] y, int K) {
+		int n = x.length;
+		long minside = 1L << 34;
+		for (int i = 0; i < n; i++)
+			for (int j = i; j < n; j++) {
+				// j==i place lower left corner at point[i]
+				// j>i incorporate both point[i] and point[j]
+				long minx = Math.min(x[i], x[j]) - 1;
+				long miny = Math.min(y[i], y[j]) - 1;
+				long[] side = new long[n];
+				int index = 0;
+				for (int k = 0; k < n; k++)
+					if (x[k] > minx && y[k] > miny)
+						side[index++] = Math.max(x[k] - minx, y[k] - miny) + 1;
+				if (index < K)
+					continue;
+				Arrays.sort(side, 0, index);
+				minside = Math.min(minside, side[K - 1]);
+			}
+		return minside * minside;
+	}
+
+}
