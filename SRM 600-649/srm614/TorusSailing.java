89bcbe901f1b5504aedde452d23c0137abd7776e
safarisoul <shenlu.wang@gmail.com>
2014-03-30 13:10:37 +1100

    srm614 div1 1000

---
+++ SRM 600-649/srm614/TorusSailing.java
@@ -0,0 +1,21 @@
+package srm614;
+
+public class TorusSailing {
+
+	public double expectedTime(int N, int M, int goalX, int goalY) {
+		double[][] dp = new double[N][M];
+		double pre = 0;
+		while (true) {
+			for (int i = 0; i < N; i++)
+				for (int j = 0; j < M; j++)
+					if (i != 0 || j != 0)
+						dp[i][j] = (dp[(i + N - 1) % N][j] + dp[i][(j + M - 1)
+								% M]) * 0.5 + 1;
+			if (Math.abs(dp[goalX][goalY] - pre) < 1e-9)
+				break;
+			pre = dp[goalX][goalY];
+		}
+		return dp[goalX][goalY];
+	}
+
+}
