c8530005761cb745c50035e0cbb15e10c1b21ec7
safarisoul <shenlu.wang@gmail.com>
2014-04-05 00:11:11 +1100

    srm615 div1 250

---
+++ SRM 600-649/srm615/AmebaDiv1.java
@@ -0,0 +1,23 @@
+package srm615;
+
+import java.util.HashSet;
+
+public class AmebaDiv1 {
+
+	public int count(int[] X) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < X.length; i++) {
+			int n = X[i];
+			for (int j = 0; j < X.length; j++)
+				if (n == X[j])
+					n += X[j];
+			set.add(n);
+		}
+		HashSet<Integer> impossible = new HashSet<Integer>();
+		for (int i = 0; i < X.length; i++)
+			if (!set.contains(X[i]))
+				impossible.add(X[i]);
+		return impossible.size();
+	}
+
+}
