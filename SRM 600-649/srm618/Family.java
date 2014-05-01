daeeb8b8d2036dfd411a0ab49deb382a2d446b15
safarisoul <shenlu.wang@gmail.com>
2014-05-01 22:08:23 +1000

    srm618 div1 250 500 permutation

---
+++ SRM 600-649/srm618/Family.java
@@ -0,0 +1,48 @@
+package srm618;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Family {
+
+	public String isFamily(int[] parent1, int[] parent2) {
+		int n = parent1.length;
+		int[] nodes = new int[n];
+		Arrays.fill(nodes, -1);
+		for (int i = 0; i < n; i++)
+			if (parent1[i] != -1) {
+				nodes[parent1[i]] = 0;
+				nodes[parent2[i]] = 0;
+			}
+		boolean valid = true;
+		for (int i = 0; i < n && valid; i++)
+			if (nodes[i] == 0) {
+				boolean gender = true;
+				HashSet<Integer> node = new HashSet<Integer>();
+				node.add(i);
+				nodes[i] = 1;
+				while (node.size() > 0 && valid) {
+					gender = !gender;
+					HashSet<Integer> next = new HashSet<Integer>();
+					for (int j = 0; j < n; j++)
+						if (parent1[j] != -1) {
+							if (node.contains(parent1[j])) {
+								if (nodes[parent2[j]] == 0)
+									next.add(parent2[j]);
+								else if (nodes[parent2[j]] != (gender ? 1 : 2))
+									valid = false;
+							} else if (node.contains(parent2[j])) {
+								if (nodes[parent1[j]] == 0)
+									next.add(parent1[j]);
+								else if (nodes[parent1[j]] != (gender ? 1 : 2))
+									valid = false;
+							}
+						}
+					for (int k : next)
+						nodes[k] = (gender ? 1 : 2);
+					node = next;
+				}
+			}
+		return (valid ? "Possible" : "Impossible");
+	}
+}
---
+++ SRM 600-649/srm618/LongWordsDiv1.java
@@ -0,0 +1,28 @@
+package srm618;
+
+public class LongWordsDiv1 {
+
+	public int count(int n) {
+		long MOD = 1000000007;
+		// pattern
+		long[] dp = new long[5001];
+		dp[1] = 1;
+		dp[2] = 1;
+		for (int i = 3; i <= n; i++) {
+			// A + f(n-1) + A
+			dp[i] = dp[i - 1];
+			// A + f(j) + A + f(i-j-1) + A
+			for (int j = 1; j <= i - 2; j++) {
+				dp[i] += dp[j] * dp[i - j - 1];
+				dp[i] %= MOD;
+			}
+		}
+		// pattern * permutation(n letter)
+		for (int i = 2; i <= n; i++) {
+			dp[n] *= i;
+			dp[n] %= MOD;
+		}
+		return (int) dp[n];
+	}
+
+}
