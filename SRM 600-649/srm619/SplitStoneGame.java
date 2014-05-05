f5709e7a987233acf99a9f36dba889e4dce2a37e
safarisoul <shenlu.wang@gmail.com>
2014-05-05 23:45:56 +1000

    srm619 div1 250

---
+++ SRM 600-649/srm619/SplitStoneGame.java
@@ -0,0 +1,15 @@
+package srm619;
+
+import java.util.Arrays;
+
+public class SplitStoneGame {
+
+	public String winOrLose(int[] number) {
+		Arrays.sort(number);
+		if (number[number.length - 1] >= 2 && number.length >= 3
+				&& number.length % 2 == 1)
+			return "WIN";
+		return "LOSE";
+	}
+
+}
