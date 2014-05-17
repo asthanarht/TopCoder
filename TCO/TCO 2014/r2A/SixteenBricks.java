d8654f7c201b5385838b1b4caf0e0f8301b43886
safarisoul <shenlu.wang@gmail.com>
2014-05-18 04:09:41 +1000

    TCO2014 r2A div1 250

---
+++ TCO/TCO 2014/r2A/SixteenBricks.java
@@ -0,0 +1,17 @@
+package r2A;
+
+import java.util.Arrays;
+
+public class SixteenBricks {
+
+	public int maximumSurface(int[] height) {
+		Arrays.sort(height);
+		int num = (height[0] + height[1]) * 4
+				+ (height[2] + height[3] + height[4] + height[5]) * 2;
+		int res = (height[8] + height[9] + height[10] + height[11] + height[12]
+				+ height[13] + height[14] + height[15])
+				* 4 - num;
+		return res + 16;
+	}
+
+}
