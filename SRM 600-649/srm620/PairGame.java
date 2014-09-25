84365a09c311fdb1c74013ac60c0e6c4175f9779
Shenlu Wang <shenlu.wang@gmail.com>
2014-09-25 21:59:22 +1000

    srm620 div1 250

---
+++ SRM 600-649/srm620/PairGame.java
@@ -0,0 +1,27 @@
+package srm620;
+
+public class PairGame {
+
+	public int maxSum(int a, int b, int c, int d) {
+		while (true) {
+			if (a == 0 || b == 0 || c == 0 || d == 0)
+				return -1;
+			if (a == c && b == d) {
+				if (a == 1 && b == 1)
+					return 2;
+				return a + b;
+			}
+			if (Math.max(a, b) > Math.max(c, d)) {
+				if (a > b)
+					a -= b;
+				else
+					b -= a;
+			} else {
+				if (c > d)
+					c -= d;
+				else
+					d -= c;
+			}
+		}
+	}
+}
