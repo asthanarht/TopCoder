8a42f2a3ea324b9d3e82aef1f9e6432a62c29c20
Shenlu Wang <shenlu.wang@gmail.com>
2014-12-17 14:58:14 +1100

    srm642 div1 250

---
+++ SRM 600-649/srm642/WaitingForBus.java
@@ -0,0 +1,20 @@
+package srm642;
+
+public class WaitingForBus {
+
+	public double whenWillBusArrive(int[] time, int[] prob, int s) {
+		double[] dp = new double[200001];
+		int n = prob.length;
+		dp[0] = 1;
+		for (int i = 0; i < s; i++)
+			if (dp[i] > 0)
+				for (int j = 0; j < n; j++)
+					dp[i + time[j]] += dp[i] * prob[j] / 100;
+		double res = 0;
+		for (int i = s; i <= 200000; i++)
+			if (dp[i] > 0)
+				res += (i - s) * dp[i];
+		return res;
+	}
+
+}
