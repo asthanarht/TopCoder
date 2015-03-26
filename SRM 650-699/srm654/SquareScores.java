83dbeb9450aac4d378c6f5f65e2b1fe8366a9b7e
Shenlu Wang <shenlu.wang@gmail.com>
2015-03-26 17:51:04 +1100

    srm654 div1 250 450 maximum subarray

---
+++ SRM 650-699/srm654/SquareScores.java
@@ -0,0 +1,29 @@
+package srm654;
+
+public class SquareScores {
+
+	public double calcexpectation(int[] p, String s) {
+		int n = s.length();
+		double[] pr = new double[26];
+		for (int i = 0; i < p.length; i++)
+			pr[i] = p[i] / 100.0;
+		char[] str = s.toCharArray();
+		double[][][] dp = new double[n][n][26];
+		for (int i = 0; i < n; i++)
+			if (str[i] == '?')
+				for (int le = 0; le < 26; le++)
+					dp[i][i][le] = pr[le];
+			else
+				dp[i][i][str[i] - 'a'] = 1;
+		double res = n;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++)
+				for (int le = 0; le < 26; le++) {
+					dp[i][j][le] += dp[i][j - 1][le] * dp[j][j][le];
+					res += dp[i][j][le];
+				}
+		}
+		return res;
+	}
+
+}
---
+++ SRM 650-699/srm654/SuccessiveSubtraction2.java
@@ -0,0 +1,49 @@
+package srm654;
+public class SuccessiveSubtraction2 {
+
+	public int[] calc(int[] a, int[] p, int[] v) {
+		int[] res = new int[p.length];
+		for (int i = 0; i < p.length; i++) {
+			a[p[i]] = v[i];
+			res[i] = go(a);
+		}
+		return res;
+	}
+
+	private int go(int[] a) {
+		int n = a.length;
+
+		int sum = a[0];
+		for (int i = 1; i < n; i++)
+			sum -= a[i];
+
+		int[] presum = new int[n];
+		int[] premin = new int[n];
+		int[] premax = new int[n];
+		for (int i = 2; i < n; i++) {
+			presum[i] = presum[i - 1] + a[i];
+			premin[i] = Math.min(premin[i - 1], presum[i]);
+			premax[i] = Math.max(premax[i - 1], presum[i] - premin[i]);
+		}
+
+		int[] sufsum = new int[n + 1];
+		int[] sufmin = new int[n + 1];
+		int[] sufmax = new int[n + 1];
+		for (int i = n - 1; i >= 2; i--) {
+			sufsum[i] = sufsum[i + 1] + a[i];
+			sufmin[i] = Math.min(sufmin[i + 1], sufsum[i]);
+			sufmax[i] = Math.max(sufmax[i + 1], sufsum[i] - sufmin[i]);
+		}
+
+		int max = 0;
+		for (int i = 2; i < n; i++) {
+			max = Math.max(max, premax[i]);
+			if (i + 2 < n)
+				max = Math.max(max, premax[i] + sufmax[i + 2]);
+			max = Math.max(max, sufmax[i]);
+		}
+
+		return max + max + sum;
+	}
+
+}
